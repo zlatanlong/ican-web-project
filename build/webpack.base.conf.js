f9da0b9af774c241e32ce4d4194b259fcbde7484
Long <long-txgc@foxmail.com>
2018-08-15 11:16:24 +0800

    解决项目部署服务器问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -69,11 +69,23 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+        // loader: 'url-loader',
+        // options: {
+        //   limit: 10000,
+        //   name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        // }
+        loaders: [
+          {
+            loader: path.resolve(__dirname, 'cssPathResolver')
+          },
+          {
+            loader: 'url-loader',
+            query: {
+              limit: 10000,
+              name: utils.assetsPath('icons/[name].[hash:7].[ext]'),
+            }
+          }
+        ]
       }
     ]
   },
