180bd65a7f34f059283d94357f8313b556f3064d
Long <long-txgc@foxmail.com>
2018-09-07 11:23:58 +0800

    全部引入外部 特别快

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import iView from 'iview'
-import 'iview/dist/styles/iview.css'
+// import 'iview/dist/styles/iview.css'
 
 Vue.use(VueAxios, axios)
 Vue.use(iView)
